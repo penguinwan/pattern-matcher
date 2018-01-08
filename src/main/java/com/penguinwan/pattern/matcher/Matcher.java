e0871e9e005c9943d453a807f7003dd9d97bce3c
penguinwan <penguin_wan@yahoo.com.sg>
2018-01-08 15:17:00 +0100

    playing with java 8 function

--- src/main/java/com/penguinwan/pattern/matcher/Matcher.java
+++ src/main/java/com/penguinwan/pattern/matcher/Matcher.java
@@ -2,6 +2,7 @@ package com.penguinwan.pattern.matcher;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Matcher {
@@ -35,6 +36,72 @@ public class Matcher {
         }
 
         return Answer.NO_MATCH;
+    }
+
+    public Answer functionalLowPerform(HashMap inputs) {
+        for (Clause clause : clauses) {
+            java.util.function.Predicate andPredicate = null;
+            List<java.util.function.Predicate> functions = new ArrayList<>();
+            for (Predicate predicate : clause.getPredicates()) {
+                java.util.function.Predicate<HashMap> equalsPredicate = (hashMapInput) -> {
+                    boolean containsKey = hashMapInput.containsKey(predicate.getLeft());
+                    //System.out.println("containsKey key " + predicate.getLeft() + " result " + containsKey);
+                    if (containsKey) {
+                        boolean sameValue = hashMapInput.get(predicate.getLeft()).equals(predicate.getRight());
+                        //System.out.println("input " + hashMapInput.get(predicate.getLeft()) + " spec " + predicate.getRight() + " result " + sameValue);
+                        return containsKey && sameValue;
+                    } else {
+                        return false;
+                    }
+                };
+
+                if (andPredicate == null) {
+                    andPredicate = equalsPredicate;
+                } else {
+                    andPredicate = andPredicate.and(equalsPredicate);
+                }
+            }
+            andPredicate.test(inputs);
+
+            //System.out.println("result " + andPredicate.test(inputs));
+        }
+        return Answer.NO_MATCH;
+    }
+
+    public Answer functionalHighPerform(HashMap inputs) {
+        java.util.function.BiPredicate andPredicate = null;
+        Clause specClause = clauses.get(0);
+        for (Predicate predicate : specClause.getPredicates()) {
+            java.util.function.BiPredicate<HashMap, HashMap> equalsPredicate = (hashMapSpec, hashMapInput) -> {
+                    String key = predicate.getLeft();
+                    if(hashMapSpec.containsKey(key) && hashMapInput.containsKey(key)) {
+                        boolean sameValue = hashMapInput.get(key).equals(hashMapSpec.get(key));
+                        //System.out.println("sameValue "+sameValue);
+                        return sameValue;
+                    } else {
+                        //System.out.println("containsKey false "+key);
+                        return false;
+                    }
+            };
+
+            if(andPredicate==null) {
+                andPredicate = equalsPredicate;
+            } else {
+                andPredicate = andPredicate.and(equalsPredicate);
+            }
+        }
+
+        for(Clause clause : clauses) {
+            HashMap hashMapSpec = new HashMap();
+            for(Predicate predicate : clause.getPredicates()) {
+                hashMapSpec.put(predicate.getLeft(), predicate.getRight());
+            }
+            //System.out.println("result "+andPredicate.test(hashMapSpec, inputs));
+            andPredicate.test(hashMapSpec, inputs);
+        }
+
+        return Answer.NO_MATCH;
+
     }
 
 }
