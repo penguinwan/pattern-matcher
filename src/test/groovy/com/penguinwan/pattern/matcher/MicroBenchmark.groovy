bedc7a9032c76caf7c16cb1b0e1e4d32829ec022
penguinwan <penguin_wan@yahoo.com.sg>
2018-01-08 15:14:16 +0100

    playing with java 8 function

---
+++ src/test/groovy/com/penguinwan/pattern/matcher/MicroBenchmark.groovy
@@ -0,0 +1,67 @@
+package com.penguinwan.pattern.matcher;
+
+import org.junit.Test
+
+import static com.penguinwan.pattern.matcher.MatcherFactory.clause
+import static com.penguinwan.pattern.matcher.MatcherFactory.newMatcher
+import static com.penguinwan.pattern.matcher.MatcherFactory.predicate
+import static com.penguinwan.pattern.matcher.MatcherFactory.predicate;
+
+class MicroBenchmark {
+    @Test
+    void "normal"() {
+        long startTime = System.currentTimeMillis()
+        100000.times {
+            Matcher matcher = newMatcher(
+                    clause().given(
+                            predicate().left("animal").right("penguin").build(),
+                            predicate().left("time").right("night").build()
+                    ).then(new Answer("place", "igloo"))
+            )
+
+            Answer answer = matcher.match(new Input("animal", "penguin"), new Input("time", "night"))
+        }
+        long endTime = System.currentTimeMillis()
+        println(endTime - startTime)
+    }
+
+    @Test
+    void "low performance"() {
+        long startTime = System.currentTimeMillis()
+        100000.times {
+            Matcher matcher = newMatcher(
+                    clause().given(
+                            predicate().left("animal").right("penguin").build(),
+                            predicate().left("time").right("night").build()
+                    ).then(new Answer("place", "igloo"))
+            )
+            HashMap input = [
+                    "animal": "penguin",
+                    "time"  : "night"
+            ]
+            matcher.functionalLowPerform(input)
+        }
+        long endTime = System.currentTimeMillis()
+        println(endTime - startTime)
+    }
+
+    @Test
+    void "high performance"() {
+        long startTime = System.currentTimeMillis()
+        100000.times {
+            Matcher matcher = newMatcher(
+                    clause().given(
+                            predicate().left("animal").right("penguin").build(),
+                            predicate().left("time").right("night").build()
+                    ).then(new Answer("place", "igloo"))
+            )
+            HashMap input = [
+                    "animal": "penguin",
+                    "time"  : "night"
+            ]
+            matcher.functionalHighPerform(input)
+        }
+        long endTime = System.currentTimeMillis()
+        println(endTime - startTime)
+    }
+}
